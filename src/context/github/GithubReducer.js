c9bf90747d1fa27ff3b53bf943516fff90dd2d45
ediaz13 <diazemanuel27@gmail.com>
2024-10-27 20:13:49 -0300

    Reducers & useReducer Hook

---
+++ src/context/github/GithubReducer.js
@@ -0,0 +1,10 @@
+const githubReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_LOADING':
+      return { ...state, loading: true };
+    case 'GET_USERS':
+      return { ...state, users: action.payload, loading: false };
+    default:
+      return state;
+  }
+}
