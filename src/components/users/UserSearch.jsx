43d37eb2ac9fc64e517b5dac01b30951a96c72f4
ediaz13 <diazemanuel27@gmail.com>
2024-10-29 14:37:50 -0300

    User Search Component

---
+++ src/components/users/UserSearch.jsx
@@ -0,0 +1,75 @@
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
+
+function UserSearch() {
+  const [text, setText] = useState("");
+  
+  const { users } = useContext(GithubContext);
+  
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (text === "") {
+      alert("Please enter something");
+      return;
+    } else {
+        // Call the searchUsers function from the parent component
+        // and pass the text as an argument
+        //searchUsers(text
+    }
+
+    console.log(text);
+  }
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Search Users..."
+                className="w-full pr-10 pl-4 py-2 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500"
+                value={text}
+                onChange={handleChange}
+              />
+              <button
+                type="submit"
+                className="absolute right-0 top-0 mt-2 mr-2"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M15 13a8 8 0 1 0-2 2l5 5"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      if {users.length > 0 && (
+        <div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
+          Clear
+        </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserSearch;
